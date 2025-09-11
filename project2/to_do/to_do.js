5950bfbb181eb08cdb82dacdc834fa4c31a62494
Marshyswallow <v3331587@gmail.com>
2025-09-11 09:13:25 +0530

    switch case

--- project2/to_do/to_do.js
+++ project2/to_do/to_do.js
@@ -1,21 +1,34 @@
-const todoInput=document.getElementById("#todo_input")
-const todoButton=document.getElementById("#add_task")
-const todoList=document.getElementById("#todo_list")
-let tasks=[];
-todoButton.addEventListener('click',()=>{
-  let taskValue=todoButton.ariaValueMax().trim();
-  if(taskValue=="")return;
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo_input");
+  const todoButton = document.getElementById("add_task");
+  const todoList = document.getElementById("todo_list");
 
-  const newTask ={
-    id:Date.now(),
-    task:taskValue,
-    completed:false,
-    
+
+
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task) => renderTask(task));
+
+  // adding task
+  todoButton.addEventListener("click", () => {
+    const taskValue = todoInput.value.trim();
+    if (taskValue === "") return;
+    const newTask = {
+      id: Date.now(),
+      task: taskValue,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    todoInput.value = "";
+  });
+
+  // managing locale storage
+  function renderTask(task) {
+    console.log(task);
   }
-  task.push(newTask)
 
-
-
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
-)
-
+});
