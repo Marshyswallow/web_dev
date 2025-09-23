ba78658c1a6cace115845f58f7958dea4de93da2
Marshyswallow <v3331587@gmail.com>
2025-09-23 15:14:17 +0530

    var help in declaring multiple variable and let is  only one

---
+++ college/assig7/script.js
@@ -0,0 +1,36 @@
+const tv=[
+  {name:"Samsung",
+  type:"LED",
+  price:45000,},
+  {name:"LG",
+  type:"OLED",
+  price:55000,},
+  {name:"Sony",
+  type:"QLED",
+  price:65000,},
+];
+
+const phone=[
+  {name:"OnePlus",
+  type:"Smartphone",
+  price:30000,
+  },  
+  {name:"Apple",
+  type:"iPhone",
+  price:70000,},
+  {name:"Samsung",
+  type:"Smartphone",
+  price:35000,},  
+];
+
+const game=[
+  {name:"Sony",
+  type:"PlayStation",
+  price:40000,},
+  {name:"Microsoft",
+  type:"Xbox",
+  price:35000,},  
+  {name:"Nintendo",
+  type:"Switch",
+  price:30000,},    
+];
